5339afde6cf0a0c2285e5f815c3e1873de9c37a2
Md. Riad Hasan <mdriadhasan2003@gmail.com>
2026-02-01 01:45:53 +0600

    Add files via upload

---
+++ Binary Number System/001_binToDec.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void binToDec(int binNum){
+    int n = binNum;
+    int decNum = 0;
+    int pow = 1; // 2^0  2^1  2^2  ........
+
+    while(n > 0){
+        int lastDig = n % 10;
+        decNum += lastDig * pow;
+        pow = pow * 2;
+        n = n / 10;
+    }
+
+    cout << decNum << endl;
+
+}
+
+int main(){
+
+    int N;
+    cout << "Enter a Binary Number : ";
+    cin >> N;
+
+    cout << "Decimal Number of " << N << " is : ";
+    binToDec(N);
+
+
+    return 0;
+}
