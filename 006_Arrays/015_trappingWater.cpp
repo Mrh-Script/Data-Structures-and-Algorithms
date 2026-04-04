80cfa6bc0910fba9de884496bf7a806848470cc1
Md. Riad Hasan <mdriadhasan2003@gmail.com>
2026-04-05 01:17:36 +0600

    Add files via upload

---
+++ 006_Arrays/015_trappingWater.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int trap(int *heights, int n){
+    int leftMax[20000], rightMax[20000];
+
+    leftMax[0] = heights[0];
+    rightMax[n-1] = heights[n-1];
+
+    //left Max Auxiliary Array
+    for(int i=1; i<n; i++){
+        leftMax[i] = max(leftMax[i-1], heights[i-1]);
+    }
+
+    //right Max Auxiliary Array
+    for(int i=n-2; i>=0; i--){
+        rightMax[i] = max(rightMax[i+1], heights[i+1]);
+    }
+
+    int waterTrapped = 0;
+    for(int i=0; i<n; i++){
+        int currentWater = min(leftMax[i], rightMax[i]) - heights[i];
+
+        if(currentWater > 0){
+            waterTrapped += currentWater;
+        }
+    }
+
+    cout << "Water Trapped : " << waterTrapped << endl;
+    return waterTrapped;
+}
+
+int main(){
+
+    int heights[7] = {4, 2, 0, 6, 3, 2, 5};
+    int n = sizeof(heights) / sizeof(int);
+
+    trap(heights, n);
+
+    return 0;
+}
+
+// Three loops used, So, TC: O(n+n+n) = O(3n) = O(n)
