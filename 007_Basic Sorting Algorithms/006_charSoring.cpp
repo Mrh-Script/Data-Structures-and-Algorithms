32653411143a8732fe49b1b26af11567e672d15c
Md. Riad Hasan <mdriadhasan2003@gmail.com>
2026-04-07 00:57:06 +0600

    Add files via upload

---
+++ 007_Basic Sorting Algorithms/006_charSoring.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+//Using Insertion Sort 
+
+void printArray(char ch[], int n){
+    cout << "Sorted Characters : ";
+    for(int i=0; i<n; i++){
+        cout << ch[i] << " ";
+    }
+    cout << endl;
+}
+
+void charSort(char ch[], int n){
+    for(int i=1; i<n; i++){
+        int current = ch[i];
+        int previous = i-1;
+
+        while(previous >= 0 && ch[previous] < current){     // In Descending Order
+            swap(ch[previous] , ch[previous + 1]);
+            previous--;
+        }
+        ch[previous + 1] = current;
+    }
+
+    printArray(ch, n);
+}
+
+int main(){
+    char ch[] = {'f', 'b', 'a', 'e', 'c', 'd'};
+    int n = sizeof(ch) / sizeof(char);
+
+    charSort(ch, n);
+
+    return 0;
+}
