7d4c94dfda60fa4c4d83cf03c114c53781c24db3
Md. Riad Hasan <mdriadhasan2003@gmail.com>
2026-04-07 00:20:30 +0600

    Add files via upload

---
+++ 007_Basic Sorting Algorithms/004_countingSort.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+void printArray(int arr[], int n){
+    cout << "Sorted Array : ";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void countingSort(int arr[], int n){
+
+    int freq[100000] = {0};  //range
+
+    int minVal = INT_MAX, maxVal = INT_MIN;
+
+    // find min and max
+    for(int i=0; i<n; i++){
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    //1st step -- O(n)
+    for(int i=0; i<n; i++){
+        freq[arr[i]]++;
+    }
+
+    //2nd step -- O(range) = max - min
+    for(int i=minVal, j=0; i<maxVal; i++){
+        while(freq[i] > 0){
+            arr[j++] = i;;
+            freq[i]--;
+        }
+    }
+    printArray(arr, n);
+}
+
+int main(){
+    int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+    int n = sizeof(arr) / sizeof(int);
+
+    countingSort(arr, n);
+
+    return 0;
+}
